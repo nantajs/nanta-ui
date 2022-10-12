04b285ecc27d0589634c550af834f76f5af5387c
aborn <loveaborn@foxmail.com>
2022-10-12 13:23:46 +0800

    feta: AppProvider

--- playground/src/App.vue
+++ playground/src/App.vue
@@ -1,12 +1,14 @@
 <template>
   <ConfigProvider>
+    <AppProvider>
       <RouterView />
+    </AppProvider>
   </ConfigProvider>
 </template>
 
 <script lang="ts" setup>
 import { ConfigProvider } from 'ant-design-vue';
-
+import { AppProvider } from '/@/components/Application';
 // Listening to page changes and dynamically changing site titles
 
 </script>
---
+++ playground/src/components/Application/index.ts
@@ -0,0 +1,16 @@
+
+import appProvider from './src/AppProvider.vue';
+import type { App, Plugin } from 'vue';
+
+export const withInstall = <T>(component: T, alias?: string) => {
+    const comp = component as any;
+    comp.install = (app: App) => {
+        app.component(comp.name || comp.displayName, component);
+        if (alias) {
+            app.config.globalProperties[alias] = component;
+        }
+    };
+    return component as T & Plugin;
+};
+
+export const AppProvider = withInstall(appProvider);
