25a993a576237e432597d98b988263a43120abbe
aborn <loveaborn@foxmail.com>
2022-10-13 20:22:57 +0800

    feat: add unplugin-icons && @iconify/json

--- playground/vite.config.ts
+++ playground/vite.config.ts
@@ -2,10 +2,11 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import path from 'path'
 import vueJsx from '@vitejs/plugin-vue-jsx'
+import Icons from 'unplugin-icons/vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(), vueJsx()],
+  plugins: [vue(), vueJsx(), Icons({ /* options */ }),],
   resolve: {
     alias: {
       "/@": path.resolve(__dirname, "./src"),
