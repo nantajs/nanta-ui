f6bbd19d2cd0b497ee7dbf0ce124d460fef3d362
aborn <loveaborn@foxmail.com>
2022-10-13 12:32:13 +0800

    feat: add icons

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,9 @@
 import NantaButton from './nanta-ui/button/NantaButton.vue'
 import NantaForm from './nanta-ui/NantaForm.vue'
 import NantaModal from './nanta-ui/NantaModal.vue'
-import { Icon, IconPicker, SvgIcon } from './nanta-ui/icon/index'
+import { Icon, SvgIcon } from './nanta-ui/icon/index'
 
-export { NantaButton, NantaForm, NantaModal, Icon, IconPicker, SvgIcon }
+export { NantaButton, NantaForm, NantaModal, Icon, SvgIcon }
 export * from './nanta-ui/form'
 export * from './nanta-ui/modal'
 export * from './nanta-ui/index'
