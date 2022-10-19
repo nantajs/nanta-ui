657777e4b334ed45e913ddd7f52b734edfe03612
aborn <loveaborn@foxmail.com>
2022-10-19 11:29:13 +0800

    add axios api data example.

--- playground/src/pages/components/nanta/table/index.vue
+++ playground/src/pages/components/nanta/table/index.vue
@@ -23,10 +23,54 @@
 <script lang="ts" setup>
 import { NantaTable, NantaTableAction, useTable } from "/~/main";
 import { columns, data } from "./data"
+import { createAxiosFetch } from '/@/utils/http/axiosFetch';
+// import { url } from '/@/settings/localSetting';
+const url = 'https://mock.data/api/mock/meta';
+
+interface DemoResult {
+    createBy?: string;
+    updateBy?: string;
+    createTime?: number;
+    updateTime?: number;
+    id: number;
+    articleid: string;
+    title: string;
+    desc: string;
+    tags?: string;
+    space?: string;
+    category?: string;
+    refArticleid?: string;
+}
+
+function transfer(params: DemoResult[]) {
+    const tData = params.map((item) => {
+        return {
+            key: item.articleid,
+            name: item.title,
+            age: item.id,
+            email: item.space,
+            address: item.desc.substring(0, 10),
+            tags: [item.tags],
+            gender: 1,
+        }
+    })
+    console.log(tData)
+    return tData;
+}
+
+const fetchSetting = {
+    pageField: 'page',
+    sizeField: 'pageSize',
+    listField: 'content',
+    totalField: 'totalElements',
+};
 
 const [registerTable] = useTable({
     columns,
-    dataSource: data,
+    // dataSource: data,
+    api: createAxiosFetch(url),
+    afterFetch: transfer,
+    fetchSetting,
     actionColumn: {
         title: '操作',
         dataIndex: 'action',
