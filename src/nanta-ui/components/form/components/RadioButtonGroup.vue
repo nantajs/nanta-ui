7a8f21805f763c546723023dcbf201b3f247c74d
aborn <loveaborn@foxmail.com>
2022-10-29 16:40:11 +0800

    feat: export propTypes

--- src/nanta-ui/components/form/components/RadioButtonGroup.vue
+++ src/nanta-ui/components/form/components/RadioButtonGroup.vue
@@ -1,6 +1,3 @@
-<!--
- * @Description:It is troublesome to implement radio button group in the form. So it is extracted independently as a separate component
--->
 <template>
   <RadioGroup v-bind="attrs" v-model:value="state" button-style="solid">
     <template v-for="item in getOptions" :key="`${item.value}`">
