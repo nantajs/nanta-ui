e67d2f4a86010f6030046d68422f445a3da5159d
aborn <loveaborn@foxmail.com>
2022-11-03 19:18:30 +0800

    github yml: Upgrade @nanta/ui to latest.

--- docs/vite.config.ts
+++ docs/vite.config.ts
@@ -4,7 +4,6 @@ import path from 'path'
 import vueJsx from '@vitejs/plugin-vue-jsx'
 import Icons from 'unplugin-icons/vite'
 import filterReplace from './build/filterReplace';
-import { version } from '../package.json'
 
 // https://vitejs.dev/config/
 export default defineConfig(({ command, mode, ssrBuild }) => {
@@ -25,13 +24,6 @@ export default defineConfig(({ command, mode, ssrBuild }) => {
             to: '@nanta/ui'
           },
         },
-        {
-          filter: /\.json$/,
-          replace: {
-            from: /#nanta-versoin/g,
-            to: version
-          },
-        }
       ],
       {
         enforce: 'pre',
