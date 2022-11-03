08c6540d1775ca8470e88e5ee67af4f73d9e8a1d
aborn <loveaborn@foxmail.com>
2022-11-03 10:57:12 +0800

    feat: add @vueuse/core deps & add ModalWrapper components.

--- src/nanta-ui/core/types/type.ts
+++ src/nanta-ui/core/types/type.ts
@@ -20,6 +20,12 @@ export declare type DynamicProps<T> = {
   [P in keyof T]: Ref<T[P]> | T[P] | ComputedRef<T[P]>;
 };
 
+declare interface ComponentElRef<T extends HTMLElement = HTMLDivElement> {
+  $el: T;
+}
+
+export declare type ComponentRef<T extends HTMLElement = HTMLDivElement> = ComponentElRef<T> | null;
+
 export declare type ElRef<T extends HTMLElement = HTMLDivElement> = Nullable<T>;
 
 export type VueNode = VNodeChild | JSX.Element;
