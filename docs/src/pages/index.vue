ef0dc490589ac86a46e625ac36da73056f6a7783
aborn <loveaborn@foxmail.com>
2022-11-03 17:00:00 +0800

    docs: show version.

--- docs/src/pages/index.vue
+++ docs/src/pages/index.vue
@@ -3,10 +3,18 @@
         <span style="font-size: 2.5rem">
             nanta-ui: Ant-design based UI framework.
         </span>
+        <div class="version-line">
+            <span>
+                Docs version: <span style="font-size: 1.5rem;color:#faad14">{{ version }} </span>
+            </span>
+            <span>
+                @nanta/ui verson: <span style="font-size: 1.5rem;color:#faad14">{{ nantVersion }}</span>
+            </span>
+        </div>
         <div class="fbox-line">
             <div v-for="item in menus" :key="item.key">
                 <NantaButton type="dashed" :icon="item.icon">
-                    <router-link :to="item.path" style="margin-left: .5rem">{{item.name}}</router-link>
+                    <router-link :to="item.path" style="margin-left: .5rem">{{ item.name }}</router-link>
                 </NantaButton>
             </div>
         </div>
@@ -17,7 +25,8 @@
 import { getMenus, getMenuList } from "/@/layouts/menu"
 import { Menu } from '/@/layouts/types/type'
 import { Icon, NantaButton } from "/~/main";
-
+import { version, dependencies } from '../../package.json'
+const nantVersion = dependencies['@nanta/ui'];
 const menus = getMenuList(getMenus());
 </script>
 
@@ -25,6 +34,14 @@ const menus = getMenuList(getMenus());
 .fbox-line {
     display: flex;
     margin-right: 1rem;
+    margin-bottom: 1rem;
+}
+
+.version-line {
+    display: flex;
+    margin-right: 1rem;
+    margin-bottom: 1rem;
+    flex-direction: column;
 }
 
 .fbox-line div {
