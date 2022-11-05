bb6f1a98da83f0286747970b873da62b169ceeed
aborn <aborn@users.noreply.github.com>
2022-11-05 15:33:09 +0000

    deploy: 32b00c6b5c53d14f385cd9395113889473b6ede2

--- assets/Exception.15bae1a4.js
+++
@@ -1,1 +0,0 @@
-import{d as r,e as o,R as a}from"./index.621635e5.js";const s=r({name:"ErrorPage",props:{title:{type:String,default:""}},setup(t){return()=>{const e="Good";return o(a,{title:t.title||e},{})}}});export{s as default};
