414fb5cd583ad616e4f4b255c4e292f4262b2ce6
aborn <aborn@users.noreply.github.com>
2022-11-05 04:51:43 +0000

    deploy: e98af647fd063a863fc7bff4a37a696259d1becd

---
+++ assets/Exception.a2456e9b.js
@@ -0,0 +1,1 @@
+import{d as r,e as o,R as a}from"./index.45ec09d1.js";const s=r({name:"ErrorPage",props:{title:{type:String,default:""}},setup(t){return()=>{const e="Good";return o(a,{title:t.title||e},{})}}});export{s as default};
--- assets/default.5bbc6a65.js
+++
@@ -1,1 +0,0 @@
-import"./index.f86152f8.js";import{B as r}from"./index.f86152f8.js";export{r as default};
