915d967782e9d8819b6380449d82df8e505b0ecd
aborn <loveaborn@foxmail.com>
2022-11-09 16:50:32 +0800

    bugfix: TypeError: Invalid attempt to destructure non-iterable instance

--- src/nanta-ui/utils/is.ts
+++ src/nanta-ui/utils/is.ts
@@ -40,6 +40,10 @@ export function isArray(val: any): val is Array<any> {
   return val && Array.isArray(val);
 }
 
+export function isString(val: unknown): val is string {
+  return is(val, 'String');
+}
+
 export function isFunction(val: unknown): val is Function {
   return typeof val === 'function';
 }
