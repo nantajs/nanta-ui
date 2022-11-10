d3f74e782eec950c306d7207f54d24ef1c84e1f1
aborn <loveaborn@foxmail.com>
2022-11-10 20:14:56 +0800

    feat: remove vite-plugin-dts vite plugin deps.

--- vite.config.ts
+++ vite.config.ts
@@ -2,13 +2,10 @@ import { resolve } from 'path'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
-import dts from 'vite-plugin-dts'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(), vueJsx(), dts({
-    insertTypesEntry: true,
-  })],
+  plugins: [vue(), vueJsx()],
   build: {
     sourcemap: true,
     lib: {
