0f1a6dce709cac96136eedfe43004fbcd9d289ca
aborn <aborn@users.noreply.github.com>
2023-02-18 14:00:35 +0000

    deploy: 1edbcdd1fd5d85d164e64c8632ca172c49d3ab5c

---
+++ assets/404-e0af7061.js
@@ -0,0 +1,1 @@
+import{d as r,f as a,R as n}from"./index-36f579ee.js";const s=r({name:"ErrorPage",props:{title:{type:String,default:""}},setup(t){return()=>{const e="404: PAGE NOT FOUND!";return a(n,{title:t.title||e},{})}}});export{s as default};
