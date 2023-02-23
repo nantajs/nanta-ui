6b74ec2628d763872ad21e8a47f2f23540a1c699
aborn <loveaborn@foxmail.com>
2023-02-23 11:24:42 +0800

    docs: nanta/ui upgrade to 0.1.4

--- docs/src/main.ts
+++ docs/src/main.ts
@@ -4,12 +4,12 @@ import App from './App.vue'
 import Antd from 'ant-design-vue';
 import 'ant-design-vue/dist/antd.css';
 import { setupRouter } from './router';
-import { setupNanta } from '@nanta/ui';
+import { NantaSetupOption, setupNanta } from '@nanta/ui';
 import '@nanta/ui/dist/style.css';
-import { NantaOption } from "@nanta/ui/dist/nanta-ui/setup";
 
 const app = createApp(App);
 setupRouter(app);
-// setupNanta(app, {locale: 'zh-CN'} as NantaOption);
-setupNanta(app,  {locale: 'en'} as NantaOption);
+
+setupNanta(app, {locale: 'zh-CN'} as NantaSetupOption);
+// setupNanta(app,  {locale: 'en'} as NantaSetupOption);
 app.use(Antd).mount('#app')
