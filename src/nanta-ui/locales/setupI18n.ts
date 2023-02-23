616730106cf1f55bbcf56f4bdfdde35386d276b5
aborn <loveaborn@foxmail.com>
2023-02-23 10:55:34 +0800

    fix: i18n: any

--- src/nanta-ui/locales/setupI18n.ts
+++ src/nanta-ui/locales/setupI18n.ts
@@ -2,7 +2,7 @@ import type { App } from 'vue';
 import { createI18n } from 'vue-i18n';
 import { I18nOptions } from 'vue-i18n'
 
-export let i18n: ReturnType<typeof createI18n>;
+export let i18n: any;
 
 export function setupI18n(app: App<Element>, options: I18nOptions) {
   i18n = createI18n(options);
