e3b69a04ac1c11199e1821021f93979d3872f41e
aborn <aborn@users.noreply.github.com>
2023-03-04 09:41:35 +0000

    deploy: b4702f94bbcc86c71539ee3266b39904167e5d12

--- assets/Exception-c60cc246.js
+++
@@ -1,1 +0,0 @@
-import{d as r,e as a,R as n}from"./index-23aa1674.js";const s=r({name:"ErrorPage",props:{title:{type:String,default:""}},setup(t){return()=>{const e="Page Not Find.";return a(n,{title:t.title||e},{})}}});export{s as default};
