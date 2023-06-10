9f04064deeb26a4bfd2e0ff2e264cacbc79dd98d
aborn <aborn@users.noreply.github.com>
2023-06-10 08:44:56 +0000

    deploy: 10a28b574cb904f4ce9342a8eece22ff8d00e978

---
+++ assets/request-5ce48bfa.js
@@ -0,0 +1,1 @@
+import{a as e}from"./axios-4a70c6fc.js";const o=e.create({timeout:8e3,headers:{"Content-Type":"application/json;charset=utf-8","Access-Control-Allow-Origin":"*"},withCredentials:!0});export{o as s};
