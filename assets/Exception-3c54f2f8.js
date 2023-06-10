83a9eb2e4bf62c0f165bb11d8ab2cd7a92d85221
aborn <aborn@users.noreply.github.com>
2023-06-10 09:15:31 +0000

    deploy: 98f63d92a877b1a86818285db423a32cc216baba

---
+++ assets/Exception-3c54f2f8.js
@@ -0,0 +1,1 @@
+import{d as r,e as a,R as n}from"./index-e543227b.js";const s=r({name:"ErrorPage",props:{title:{type:String,default:""}},setup(t){return()=>{const e="Page Not Find.";return a(n,{title:t.title||e},{})}}});export{s as default};
